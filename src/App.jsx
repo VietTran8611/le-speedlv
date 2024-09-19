3c404194d7dd98008524a40ca348f3cf2bb52261
Omnilios <omnilios123@gmail.com>
2024-09-19 17:26:55 -0400

    deploy

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import { Main } from './components/Main'
+
+
+function App() {
+
+
+  return (
+    <>
+    <form action="">
+      <input type="checkbox" id="fruit1" name="fruit-1" value="Apple"></input>
+    </form>
+      <Main />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Main.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react'
+import data from './data.json'
+
+export const Main = () => {
+    const [checkedState, setCheckedState] = useState(
+        new Array(data.length).fill(false)
+    );
+
+    const [checkedFlip, setCheckedslip] = useState(
+        new Array(data.length).fill(false)
+    );
+
+    const handleOnChange = (position) => {
+        const updatedCheckedState = checkedState.map((item, index) =>
+          index === position ? !item : item
+        );
+        setCheckedState(updatedCheckedState);
+      };
+
+
+      const handleExpand = (position) => {
+        const updatedCheckedState = checkedFlip.map((item, index) =>
+            index === position ? !item : item
+          );
+          setCheckedslip(updatedCheckedState);
+      }; 
+    return (
+        <div>
+            <ul className='list'>
+                {data.map(({title,content},index)=>{
+                    return(
+                        <li key={index}>
+                            <div className='top-section'>
+                                <div className='top-section-left'>
+                                    <input 
+                                        type="checkbox" 
+                                        name={title}
+                                        value={title} 
+                                        checked={checkedState[index]} 
+                                        id={`custom-checkbox-${index}`} 
+                                        onChange={() => handleOnChange(index)}
+                                    />
+                                    <label htmlFor={`custom-checkbox-${index}`} >{title}</label>
+                                </div>
+                                    <h1 className='expand-btn' onClick={() => handleExpand(index)}>{checkedFlip[index]? "-" : "+"}</h1>
+                                    
+                            </div>
+                            <div className= {checkedFlip[index] ? 'content show' : 'content'}>
+                                {content.map((drop)=>{
+                                    return(
+                                        <div>
+                                            <p>{drop.text}</p>
+                                            <img className='image' src={`src\\components\\img\\${drop.img}`} alt="" />
+                                        </div>
+                                    )
+                                })}
+                            </div>
+                        </li>
+                    )
+                })}
+            </ul>
+        </div>
+    )
+}
